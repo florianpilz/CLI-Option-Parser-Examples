1c410687475f915488e51d91fdcbcb9096162b96
Florian Pilz <fpilz87@googlemail.com>
2011-02-09 13:10:13 +0100

    implement getoptdeclare-example

--- getoptdeclare-example.rb
+++ getoptdeclare-example.rb
@@ -1,15 +1,19 @@
 require 'rubygems'
 require 'Getopt/Declare'
-# yields exactly thos help message if called with -h:
+require 'lib/timetabling'
+
 args = Getopt::Declare.new(<<'EOF')
 
--q, --quiet                  quiet
--f, --files <files:if>...    input files
--n, --number  <num:n>        a float number
--i, --integer <num:i>        an integer number
-
+-s, --severity <num:i>        severity of the timetabling problem
+-m, --mutation <str:s>        mutation used in algorithm, see lib/mutations.rb for options
+-r, --recombination <str:qs>   recombination used in algorithm, see lib/recombinations.rb for options
+-i, --iterations <num:i>      algorithm will stop after given iterations or run indefinitely if 0
+-t, --time-limit <num:i>      algorithm will stop after given time limit or run indefinitely if 0
+-c, --cycles <num:i>          algorithm will run that many times
 EOF
-# throws an uninformative error message when called with ruby getoptdeclare.rb -f
-p args['-q']
-p args['-f']
-p args.unused
+
+constraints = Timetabling::read_timetable_data(args['-s'])
+
+args['-c'].times do
+  Timetabling::run(:constraints => constraints, :mutation => Kernel.const_get(args['-m']).new, :recombination => Kernel.const_get(args['-r']).new, :number_of_slots => 30, :population_size => 1, :childs => 1, :recombination_chance => 0.0, :mutation_chance => 1.0, :iteration_limit => args['-i'], :time_limit => args['-t'])
+end
